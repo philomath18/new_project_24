f7e842a6f157ab203aac69a1dcd0c390b2197b0d
philomath18 <41228478+philomath18@users.noreply.github.com>
2024-11-21 11:28:29 +0530

    Update run_colab_notebook.py

--- run_colab_notebook.py
+++ run_colab_notebook.py
@@ -2,9 +2,9 @@ import papermill as pm
 import os
 
 # Path to the notebook file on GitHub
-notebook_path = "https://raw.githubusercontent.com/philomath18/new_project_24/refs/heads/main/crypto_portfolio_tracker.ipynb"  # Update with your GitHub notebook path
-#output_notebook = "https://github.com/philomath18/new_project_24/crpyto_tracker_output.ipynb"  # Name for the executed notebook output
-output_notebook = '/tmp/crypto_portfolio_updated.ipynb'
+notebook_path = "https://raw.githubusercontent.com/philomath18/new_project_24/refs/heads/main/crypto_portfolio_tracker.ipynb"
+output_notebook = './crypto_portfolio_updated.ipynb'
+
 # Run the notebook using papermill
 try:
     print(f"Running notebook: {notebook_path}")
@@ -14,5 +14,11 @@ try:
         parameters={},  # Optional: Add notebook parameters here
     )
     print(f"Notebook executed successfully. Output saved as {output_notebook}")
+
+    # Verify file creation
+    if os.path.exists(output_notebook):
+        print(f"Output notebook is available at: {output_notebook}")
+    else:
+        print("Failed to locate the output notebook.")
 except Exception as e:
     print(f"Failed to execute the notebook: {e}")
