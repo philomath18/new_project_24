8758eb8d8456b69c709dd2298b87b33ee700132a
philomath18 <41228478+philomath18@users.noreply.github.com>
2024-11-22 13:58:49 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -69,6 +69,7 @@ total_initial_value = df['value_initial'].sum() * 90
 # Calculate the total portfolio value
 df['value'] = df['value'].astype(int)
 df['value_inr'] = df['value_inr'] * 90
+df['value_inr'] = df['value_inr'].astype(int)
 
 #st.markdown(f"<h3 style='text-align: right; font-weight: bold;'>check: {total_initial_value}</h3>", unsafe_allow_html=True)
 
